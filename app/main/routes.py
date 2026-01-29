921c8b122bee2770cd071ad216dbf4528b22f997
Minji <rlaminwl02@inu.ac.kr>
2026-01-29 12:39:14 +0900

    Update changes

--- app/main/routes.py
+++ app/main/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_required, current_user
 from . import bp
 from ..extensions import db
-from ..models import Neighborhood, UserNeighborhood
+from ..models import Neighborhood, UserNeighborhood, Post, JoinRequest
 
 
 @bp.route("/")
@@ -42,3 +42,28 @@ def choose_neighborhood():
         return redirect(url_for("posts.list_posts"))
 
     return render_template("main/choose_neighborhood.html", neighborhoods=neighborhoods)
+
+
+@bp.route("/me/joins")
+@login_required
+def my_joins():
+    """내가 ACTIVE로 참여 중인 모집글 목록"""
+    joins = JoinRequest.query.filter_by(user_id=current_user.id, status="ACTIVE") \
+                             .order_by(JoinRequest.created_at.desc()).all()
+
+    post_ids = [j.post_id for j in joins]
+    posts_by_id = {}
+    if post_ids:
+        posts = Post.query.filter(Post.id.in_(post_ids)).all()
+        posts_by_id = {p.id: p for p in posts}
+
+    return render_template("main/my_joins.html", joins=joins, posts_by_id=posts_by_id)
+
+
+@bp.route("/me/posts")
+@login_required
+def my_posts():
+    """내가 작성한 모집글 목록"""
+    posts = Post.query.filter_by(author_id=current_user.id) \
+                      .order_by(Post.created_at.desc()).all()
+    return render_template("main/my_posts.html", posts=posts)
